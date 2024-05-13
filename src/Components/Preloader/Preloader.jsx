8dcf889b3f4846a1e7c2a7d2471f875f794cea22
akshat0012 <akshatchaudhary1222@gmail.com>
2024-05-14 02:24:41 +0530

    fixed preload animation

--- src/Components/Preloader/Preloader.jsx
+++ src/Components/Preloader/Preloader.jsx
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import icons from '../../icons.json'
-const Preloader = () => {
-  // State to manage the visibility of the preloader
-  const [isVisible, setIsVisible] = useState({
-    visiblity: true,
-    index: 100
-  });
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import icons from '../../icons.json';
 
-  // Simulate loading delay and hide the preloader after a certain time
+const Preloader = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsVisible({
-        index: -1,
-        visiblity: false
-      });
-    }, 3700); // Change this value to adjust the delay before the preloader fades out
+      setIsVisible(false);
+    }, 3700);
+
     return () => {
-        clearTimeout(timer);
-        console.log(isVisible.visiblity, " ", isVisible.index);
-    }
+      clearTimeout(timer);
+    };
   }, []);
 
   return (
-    <section className={`absolute top-0 z-[100]`}>
-      <div className={`flex flex-col items-center justify-center bg-black h-screen w-screen text-white text-xl ${isVisible.visiblity ? 'opacity-100' : 'opacity-0 transition-opacity duration-700'}`}>
+    <AnimatePresence >
+    {isVisible && 
+    <motion.div className={`absolute top-0 z-[100]`}
+        initial={{ opacity: 1 }}
+        transition = {{
+          delay: 0.6 
+        }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+    >
+      <div className={`flex flex-col items-center justify-center bg-black h-screen w-screen text-white text-xl`}>
         {/* Your preloader content */}
         <img src={icons['Windows_logo']} width="90px" alt="Windows logo"/>
         <h1 className="mt-5">This might take a few minutes.</h1>
         <h2>Don't turn off your PC</h2>
       </div>
-    </section>
+    </motion.div>}
+    </AnimatePresence>
   );
 };
 
